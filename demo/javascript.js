347eb1937c6238e8a682ef4a85d66bbe48af7765
b1mind <brent@1mindsolutions.com>
2021-01-22 01:10:36 -0700

    🦈 more test content

--- demo/javascript.js
+++ demo/javascript.js
@@ -3,28 +3,33 @@ export default async function unreachableCode() {
   return 10;
 }
 
+//never used
 const hello = "I am never used";
-
 doesNotExist.nope(`I'm never imported`);
-
 nothing.call;
 
 class Widget extends React.Component {}
-
 Widget;
 
-const mindArrows = () => {};
+const someNew = new Date().getDate();
+console.log(someNew.checking);
+
+function mindArrows() {}
 const brent = function () {};
 
 function functionJunction(e, params, ...args) {
+  params.someShit;
   e.target = this.thing;
   let args = args;
-  const params = params;
-  console.log(params);
+  const paramsConst = params;
+  brent;
+  console.log(params, paramsConst);
 }
 
 const lastName = "Morton";
 const age = 200;
+
+functionJunction();
 
 console.log(brent, lastName);
 
@@ -33,13 +38,12 @@ let variable = "this is a variable";
 console.log(`this is a ${variable}`);
 
 const constant = `some things need to be colored ${variable2}`;
+
 console.log(constant, variable);
 
 if (age !== 40 || variable2 === 420) {
   console.log("you are not the right age");
 }
-const someNew = new Date().getDate();
-console.log(someNew);
 
 const myObj = {
   prop: 12,
@@ -57,12 +61,11 @@ const myObj = {
 myObj.addProps(age);
 
 const turneryOps = age === 100 ? "old person" : "not that old";
-
 turneryOps;
 
 const wesConst = {
-  age: 100,
-  name: `Wes`,
+  age: 120,
+  name: `Brent`,
 };
 
 console.log(wesConst.age);
