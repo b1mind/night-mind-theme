b5eb32a0ad2ff09b38a5d9824d75823f1f395072
b1mind <brent@1mindsolutions.com>
2021-01-21 17:25:20 -0700

    😜 demo files added

---
+++ demo/javascript.js
@@ -0,0 +1,71 @@
+import { nothing } from "nothing";
+export default async function unreachableCode() {
+  return 10;
+}
+const hello = "I am never used";
+
+doesNotExist.nope(`I'm never imported`);
+
+class widget extends React.Component {}
+
+const joe = function () {};
+
+const wes = 100;
+jf;
+
+function wes() {}
+
+const wes = () => {};
+
+console.log(joe, wes);
+let variable = "this is a variable";
+
+let variable2 = 420;
+
+console.log(`this is a ${variable}`);
+
+const string = `some things need to be colored ${variable2}`;
+console.log(string);
+
+const age = 200;
+if (age !== 40 || variable2 === 420) {
+  console.log("you are not the right age");
+}
+
+age = 100;
+const myObj = {
+  prop: 12,
+  nested: {
+    prop: 100,
+  },
+  arraySure: [a, b, c, d],
+  initialAge: age,
+  age,
+  addProps: function (e, index) {
+    return prop + nested.prop + e;
+  },
+};
+
+myObj.addProps(age);
+
+const turneryOps = age === 100 ? "old person" : "not that old";
+
+turneryOps;
+
+function functionJunction(e, params, ...args) {
+  e.target = this.thing;
+  let args = args;
+}
+
+functionJunction();
+
+const wes = {
+  age: 100,
+  name: `Wes`,
+};
+
+console.log(wes.cool);
+console.log(wes.cool);
+
+console.log(wes.age);
+console.log(wes.doesNotExist);
---
+++ demo/svelte.svelte
@@ -0,0 +1,23 @@
+<script>
+  const comeOn = "this should work";
+  function someFun(e) {
+    console.log(e);
+  }
+  console.log(comeOn);
+</script>
+
+<main>
+  <div class="class">
+    <div id="id" />
+
+    
+  </div>
+</main>
+
+<style type="text/scss">
+  //style like its 1999
+
+  main {
+    color: white;
+  }
+</style>
